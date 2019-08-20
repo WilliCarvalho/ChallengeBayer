9b2a7fc00361531b30cd537b21b05d29bbaa4edc
Renan Paim Grell <renanpaimgrellrpg@hotmail.com>
2019-08-19 23:15:12 -0300

    gameplay mechanics prototype 1

---
+++ Bayer/Assets/Scripts/SwapTexture.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapTexture : MonoBehaviour
+{
+    public Texture[] texturas;
+    public int currentTexture;
+    public bool iluminado = false;
+    public GameObject SpawnPoint;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void trocaTextura()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            currentTexture++;
+            currentTexture %= texturas.Length;            
+            GetComponent<Renderer>().material.mainTexture = texturas[currentTexture];
+            iluminado = true;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (iluminado == false)
+        {
+            //Destroy(other.gameObject);
+            //Instantiate(other, SpawnPoint.transform.position, transform.rotation);
+            other.transform.position = SpawnPoint.transform.position;
+            
+        }
+    }
+}
